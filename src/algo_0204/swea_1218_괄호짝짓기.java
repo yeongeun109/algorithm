efb2fdb8f2da40b19b0b41ab663658dd9708dcd0
윤영은 <ounog@naver.com>
2021-08-11 22:14:30 +0900

    init

---
+++ src/algo_0204/swea_1218_괄호짝짓기.java
@@ -0,0 +1,43 @@
+package algo_0204;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class swea_1218_괄호짝짓기 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		for(int i = 0; i < 10; i++) {
+			int T = Integer.parseInt(in.readLine());
+			String str = in.readLine();
+			Stack<Character> stack = new Stack<Character>();
+
+			for(int t = 0; t < T; t++) {
+				if(!stack.isEmpty()) {
+					if(stack.peek() == '<' && str.charAt(t) == '>') {
+						stack.pop();
+					}else if(stack.peek() == '[' && str.charAt(t) == ']') {
+						stack.pop();
+					}else if(stack.peek() == '(' && str.charAt(t) == ')') {
+						stack.pop();
+					}else if(stack.peek() == '{' && str.charAt(t) == '}') {
+						stack.pop();
+					}else {
+						stack.push(str.charAt(t));
+					}
+				}else {
+					stack.push(str.charAt(t));
+				}
+			}
+			if(stack.isEmpty())
+				System.out.println("#" + (i+1) + " 1");
+			else
+				System.out.println("#" + (i+1) + " 0");
+		}
+		
+	}
+
+}
