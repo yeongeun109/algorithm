49fcc2ee46f96b65eb3ab7e2fb1ca7260cc28a16
윤영은 <ounog@naver.com>
2021-08-23 22:07:20 +0900

    test

---
+++ algo/src/algo_0823/bj_1138_한줄로서기.java
@@ -0,0 +1,27 @@
+package algo_0823;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class bj_1138_한줄로서기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+
+		ArrayList<Integer> list = new ArrayList<>();
+		int[] line = new int[N];
+		for (int i = 0; i < N; i++) {
+			line[i] = sc.nextInt();
+		}
+
+		for (int i = N - 1; i >= 0; i--) {
+			list.add(line[i], i + 1);
+		}
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.print(list.get(i) + " ");
+		}
+	}
+
+}
