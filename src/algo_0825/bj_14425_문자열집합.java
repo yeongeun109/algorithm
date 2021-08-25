4cc462a46685a9349320d4af252d18924ccd7810
윤영은 <ounog@naver.com>
2021-08-25 19:35:21 +0900

    bj 14425 문자열집합

---
+++ src/algo_0825/bj_14425_문자열집합.java
@@ -0,0 +1,34 @@
+package algo_0825;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class bj_14425_문자열집합 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		String[] arr = new String[N];
+		int res = 0;
+		
+		for(int i = 0; i < N; i++) {
+			arr[i] = in.readLine();
+		}
+		
+		for(int i = 0; i < M; i++) {
+			String str = in.readLine();
+			for(int j = 0; j < N; j++) {
+				if(arr[j].equals(str)) {
+					res++;
+					break;
+				}
+			}
+		}
+		System.out.println(res);
+	}
+
+}
