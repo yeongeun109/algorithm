ed429316d299c95ac8165f4814fb5b29a9b726ce
윤영은 <ounog@naver.com>
2021-09-05 23:17:56 +0900

    이름수정

--- src/algo_202109/구현_pg_기둥과보설치_미해결.java
+++ src/algo_202109/구현_pg_기둥과보설치_미해결.java
@@ -5,7 +5,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
-public class 구현_pg_기둥과보설치 {
+public class 구현_pg_기둥과보설치_미해결 {
 
 	public static void main(String[] args) {
 		solution(5, new int[][] {{1,0,0,1},{1,1,1,1,},{2,1,0,1},{2,2,1,1},{5,0,0,1},{5,1,0,1},{4,2,1,1},{3,2,1,1}});
