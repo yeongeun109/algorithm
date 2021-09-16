da83ea32cf6a3cd988146d928717a763ea0ff580
윤영은 <ounog@naver.com>
2021-09-17 01:25:55 +0900

    bj 11726 2xn 타일링 (DP)

---
+++ src/algo_202109/DP_bj_11726_2xn타일링.java
@@ -0,0 +1,22 @@
+package algo_202109;
+
+import java.util.Scanner;
+
+public class DP_bj_11726_2xn타일링 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] dp = new int[n + 1];
+		dp[1] = 1; 
+		
+		if(n > 1) {
+			dp[2] = 2;
+			for(int i = 3; i <= n; i++) {
+				dp[i] = (dp[i - 1] + dp[i - 2]) % 10007;
+			}
+		}
+		System.out.println(dp[n]);
+	}
+
+}
