b4596f27d50cb450fcfb500c598994607477c6ec
윤영은 <ounog@naver.com>
2021-12-02 14:01:58 +0900

    bj 11725 트리의 부모 찾기(트리, dfs)

---
+++ src/algo_202111/트리_bj_11725_트리의부모찾기.java
@@ -0,0 +1,55 @@
+package algo_202111;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class 트리_bj_11725_트리의부모찾기 {
+
+	static List<Integer>[] list;
+	static boolean[] visited;
+	static boolean flag = false;
+	static int[] res;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(in.readLine());
+		list = new ArrayList[N + 1];
+		res = new int[N + 1];
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 1; i <= N; i++) {
+			list[i] = new ArrayList<Integer>();
+		}
+
+		for (int i = 1; i < N; i++) {
+			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			list[a].add(b);
+			list[b].add(a);
+		}
+		visited = new boolean[N + 1];
+		dfs(1);
+		for(int i = 2; i <= N; i++) {
+			sb.append(res[i]).append("\n");
+		}
+		System.out.println(sb);
+	}
+
+	private static void dfs(int n) {
+		visited[n] = true;
+		
+		for(int i = 0; i < list[n].size(); i++) {
+			if(!visited[list[n].get(i)]) {
+				res[list[n].get(i)] = n;
+				dfs(list[n].get(i));
+			}
+		}
+	}
+
+}
