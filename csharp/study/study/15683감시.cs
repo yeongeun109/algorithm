c715e7cdcb459898063f2c64dd0570e3faab0639
yeongeun109 <ounog@naver.com>
2022-02-09 09:51:41 +0900

    add csharp

---
+++ csharp/study/study/15683감시.cs
@@ -0,0 +1,202 @@
+﻿namespace HelloWorld
+{
+    class Program
+    {
+        static int res, a, b;
+        static List<int[]> list = new List<int[]>();
+        static int[,] map, tmp2;
+
+        static void Main(string[] args)
+        {
+            string? str = Console.ReadLine();
+            a = Convert.ToInt32(str.Split(" ")[0]);
+            b = Convert.ToInt32(str.Split(" ")[1]);
+            res = a * b;
+            map = new int[a, b];
+
+            for (int i = 0; i < a; i++)
+            {
+                str = Console.ReadLine();
+                string[] tmp = str.Split(" ");
+                for (int j = 0; j < b; j++)
+                {
+                    map[i, j] = Convert.ToInt32(tmp[j]);
+                    if (map[i, j] != 0)
+                    {
+                        list.Add(new int[] { i, j, map[i, j] });
+                    }
+                }
+            }
+
+            if (list.Count != 0)
+                dfs(map, 0);
+
+            Console.WriteLine(res);
+        }
+
+        static void up(int cx, int cy)
+        {
+            for (int i = cx - 1; i >= 0; i--)
+            {
+                if (tmp2[i, cy] == 6)
+                    break;
+                else if (tmp2[i, cy] == 0)
+                    tmp2[i, cy] = -1;
+            }
+        }
+        static void down(int cx, int cy)
+        {
+            for (int i = cx + 1; i < a; i++)
+            {
+                if (tmp2[i, cy] == 6)
+                    break;
+                else if (tmp2[i, cy] == 0)
+                    tmp2[i, cy] = -1;
+            }
+        }
+        static void left(int cx, int cy)
+        {
+            for (int i = cy - 1; i >= 0; i--)
+            {
+                if (tmp2[cx, i] == 6)
+                    break;
+                else if (tmp2[cx, i] == 0)
+                    tmp2[cx, i] = -1;
+            }
+        }
+        static void right(int cx, int cy)
+        {
+            for (int i = cy + 1; i < b; i++)
+            {
+                if (tmp2[cx, i] == 6)
+                    break;
+                else if (tmp2[cx, i] == 0)
+                    tmp2[cx, i] = -1;
+            }
+        }
+        static void dfs(int[,] before, int cnt)
+        {
+            if (cnt == list.Count)
+            {
+                int zeroCnt = 0;
+                for (int i = 0; i < a; i++)
+                {
+                    for (int j = 0; j < b; j++)
+                    {
+                        if (before[i, j] == 0)
+                            zeroCnt++;
+                    }
+                }
+                res = Math.Min(res, zeroCnt);
+                return;
+            }
+
+            int[] tmp = list.ElementAt<int[]>(cnt);
+            int cx = tmp[0];
+            int cy = tmp[1];
+            tmp2 = (int[,])before.Clone();
+
+            if (tmp[2] == 1)
+            {
+                // 상
+                up(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 하
+                tmp2 = (int[,])before.Clone();
+                down(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 좌
+                tmp2 = (int[,])before.Clone();
+                left(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 우
+                tmp2 = (int[,])before.Clone();
+                right(cx, cy);
+                dfs(tmp2, cnt + 1);
+            }
+            else if (tmp[2] == 2)
+            {
+                // 상하
+                up(cx, cy);
+                down(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 좌우
+                tmp2 = (int[,])before.Clone();
+                left(cx, cy);
+                right(cx, cy);
+                dfs(tmp2, cnt + 1);
+            }
+            else if (tmp[2] == 3)
+            {
+                // 위오
+                up(cx, cy);
+                right(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 오아
+                tmp2 = (int[,])before.Clone();
+                right(cx, cy);
+                down(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 아왼
+                tmp2 = (int[,])before.Clone();
+                down(cx, cy);
+                left(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 왼위
+                tmp2 = (int[,])before.Clone();
+                left(cx, cy);
+                up(cx, cy);
+                dfs(tmp2, cnt + 1);
+            }
+            else if (tmp[2] == 4)
+            {
+                // 왼위오
+                left(cx, cy);
+                up(cx, cy);
+                right(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 위오아
+                tmp2 = (int[,])before.Clone();
+                up(cx, cy);
+                right(cx, cy);
+                down(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 오아왼
+                tmp2 = (int[,])before.Clone();
+                right(cx, cy);
+                down(cx, cy);
+                left(cx, cy);
+                dfs(tmp2, cnt + 1);
+
+                // 아왼위
+                tmp2 = (int[,])before.Clone();
+                down(cx, cy);
+                left(cx, cy);
+                up(cx, cy);
+                dfs(tmp2, cnt + 1);
+            }
+            else if (tmp[2] == 5)
+            {
+                down(cx, cy);
+                left(cx, cy);
+                up(cx, cy);
+                right(cx, cy);
+                dfs(tmp2, cnt + 1);
+            }
+            else
+            {
+                dfs(tmp2, cnt + 1);
+            }
+        }
+
+    }
+}
---
+++ csharp/study/study/16235나무재태크.cs
@@ -0,0 +1,118 @@
+﻿namespace study
+{
+    internal class _16235나무재태크
+    {
+        static void Main(string[] args)
+        {
+            int N, M, K;
+            string[] str = Console.ReadLine().Split(" ");
+            N = int.Parse(str[0]);
+            M = int.Parse(str[1]);
+            K = int.Parse(str[2]);
+
+            int[,] food = new int[N, N];
+            int[,] arr = new int[N, N];
+            List<int>[,] tree3 = new List<int>[N, N];
+
+            for(int i = 0; i < N; i++)
+            {
+                str = Console.ReadLine().Split(" ");
+                for(int j = 0; j < N; j++)
+                {
+                    arr[i, j] = int.Parse(str[j]);
+                    food[i, j] = 5;
+                    tree3[i, j] = new List<int>();
+                }
+            }
+
+            for(int i = 0; i < M; i++)
+            {
+                str = Console.ReadLine().Split(" ");
+                tree3[int.Parse(str[0]) - 1, int.Parse(str[1]) - 1].Add(int.Parse(str[2]));
+            }
+
+
+            List<int[]> dead = new List<int[]>();
+            int[] dx = {-1, -1, -1, 0, 0, 1, 1, 1};
+            int[] dy = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+            for(int i = 0; i < K; i++)
+            {
+                // 봄
+                for (int j = 0; j < N; j++)
+                {
+                    for (int k = 0; k < N; k++)
+                    {
+                        List<int> tmp = tree3[j, k];
+                        tmp.Sort();
+                        for (int l = 0; l < tmp.Count; l++)
+                        {
+                            if (food[j, k] >= tmp[l])
+                            {
+                                food[j, k] -= tmp[l];
+                                tree3[j, k][l]++;
+                            }
+                            else
+                            {
+                                dead.Add(new int[] { j, k, tmp[l], l });
+                            }
+                        }
+                    }
+                }
+
+                // 여름
+                for(int j = dead.Count - 1; j >= 0; j--)
+                {
+                    food[dead[j][0], dead[j][1]] += (dead[j][2] / 2);
+                    tree3[dead[j][0], dead[j][1]].RemoveAt(dead[j][3]);
+                }
+                dead.Clear();
+
+                // 가을
+                for(int j = 0; j < N; j++)
+                {
+                    for(int k = 0; k < N; k++)
+                    {
+                        List<int> tmp = tree3[j, k];
+                        for (int l = 0; l < tmp.Count; l++)
+                        {
+                            if(tmp[l] % 5 == 0)
+                            {
+                                for (int m = 0; m < 8; m++)
+                                {
+                                    int tx = j + dx[m];
+                                    int ty = k + dy[m];
+                                    if (tx < 0 || tx >= N || ty < 0 || ty >= N)
+                                        continue;
+
+                                    tree3[tx, ty].Add(1);
+                                }
+                            }
+                        }
+                    }
+                }
+
+                // 겨울
+                for(int j = 0; j < N; j++)
+                {
+                    for(int k = 0; k < N; k++)
+                    {
+                        food[j, k] += arr[j, k];
+                    }
+                }
+            }
+
+
+            int cnt = 0;
+            for(int i = 0; i < N; i++)
+            {
+                for(int j = 0; j < N; j++)
+                {
+                    cnt += tree3[i, j].Count;
+                }
+            }
+
+            Console.WriteLine(cnt);
+        }
+    }
+}
