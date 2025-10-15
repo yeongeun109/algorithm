26a0359432cbcab864c09c4d3736f50e630d85c4
윤영은 <ounog@naver.com>
2025-10-16 00:57:08 +0900

    b13335 트럭(구현)

---
+++ java/src/algo_2510/b13335_트럭_구현.java
@@ -0,0 +1,60 @@
+package algo_2510;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class b13335_트럭_구현 {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(in.readLine());
+		int n = Integer.parseInt(st.nextToken()); // 트럭의 수
+		int w = Integer.parseInt(st.nextToken()); // 다리의 길이
+		int L = Integer.parseInt(st.nextToken()); // 최대하중
+		int res = 0;
+		
+		st = new StringTokenizer(in.readLine());
+		Queue<Integer> waitTruck = new LinkedList<>();
+		
+		for(int i = 0; i < n; i++) {
+			waitTruck.add(Integer.parseInt(st.nextToken()));
+		}
+		
+		//while(!waitTruck.isEmpty()) {
+		int bridgeWeight = 0;
+		int inCnt = 0;
+		int outCnt = 0;
+		Queue<Integer> bridge = new LinkedList<>(); 
+		int[] loc = new int[n];
+		int start = 0;
+		
+		for(int i = 0; i < w; i++) {
+			bridge.add(0);
+		}
+
+		while(!bridge.isEmpty()) {
+			res++;
+			
+			bridgeWeight -= bridge.poll();
+			if(!waitTruck.isEmpty()) {
+				if(bridgeWeight + waitTruck.peek() <= L) {
+					int nextTruck = waitTruck.poll();
+					bridgeWeight += nextTruck;
+					bridge.add(nextTruck);
+				}else {
+					bridge.add(0);
+				}
+			}else {
+				
+			}
+			
+		}
+		
+		System.out.println(res);
+	}
+}
